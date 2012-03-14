4836d1a119a3b42b5dc2a6c3082f18706073602f
LeonB <leon@tim-online.nl>
2012-03-14 23:36:17 +0100

    Changed how to include libraries

--- wsgi.py
+++ wsgi.py
@@ -1,8 +1,9 @@
 import sys, os
 from google.appengine.ext.appstats import recording
 
-package_dir = "gae-env"
-package_dir = "gae-env/lib/python2.7/site-packages"
+# package_dir = "gae-env"
+# package_dir = "gae-env/lib/python2.7/site-packages"
+package_dir = 'libs'
 package_dir_path = os.path.join(os.path.dirname(__file__), package_dir)
 
 # Allow unzipped packages to be imported
@@ -11,7 +12,7 @@ sys.path.insert(0, package_dir_path)
 
 # Append zip archives to path for zipimport
 for filename in os.listdir(package_dir_path):
-    if filename.endswith((".zip", ".egg")):
+    if filename.endswith((".zip", ".egg", ".tar.gz")):
         sys.path.insert(0, "%s/%s" % (package_dir_path, filename))
 
 from motorrijweer import app
